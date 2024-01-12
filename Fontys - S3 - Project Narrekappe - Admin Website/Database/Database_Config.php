958e550b279efd834a4626149966095a4c956579
Boemy <117738306+Boemy@users.noreply.github.com>
2024-01-12 11:49:43 +0100

    Delete Fontys - S3 - Project Narrekappe - Admin Website directory

--- Fontys - S3 - Project Narrekappe - Admin Website/Database/Database_Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Define Database Credentials and Name
-    $DatabaseConfig = array(
-        "DB_Server"     => "10.10.30.2",
-        "DB_Username"   => "student",
-        "DB_Password"   => '&Xn7N34NUihxQomv$F',
-        "DB_Name"       => "challenges"
-    );
-?>
